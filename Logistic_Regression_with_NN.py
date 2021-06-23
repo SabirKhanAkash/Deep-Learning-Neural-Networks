c3c5550b942c25d08b0918430b63ecb269d6dd41
Sabir Khan Akash <39434260+SabirKhanAkash@users.noreply.github.com>
2021-06-23 23:49:07 +0600

    code updated

--- Logistic_Regression_with_NN.py
+++ Logistic_Regression_with_NN.py
@@ -143,61 +143,22 @@ propagate_test(propagate)
 # GRADED FUNCTION: optimize
 
 def optimize(w, b, X, Y, num_iterations=100, learning_rate=0.009, print_cost=False):
-    """
-    This function optimizes w and b by running a gradient descent algorithm
-    
-    Arguments:
-    w -- weights, a numpy array of size (num_px * num_px * 3, 1)
-    b -- bias, a scalar
-    X -- data of shape (num_px * num_px * 3, number of examples)
-    Y -- true "label" vector (containing 0 if non-cat, 1 if cat), of shape (1, number of examples)
-    num_iterations -- number of iterations of the optimization loop
-    learning_rate -- learning rate of the gradient descent update rule
-    print_cost -- True to print the loss every 100 steps
-    
-    Returns:
-    params -- dictionary containing the weights w and bias b
-    grads -- dictionary containing the gradients of the weights and bias with respect to the cost function
-    costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
-    
-    Tips:
-    You basically need to write down two steps and iterate through them:
-        1) Calculate the cost and the gradient for the current parameters. Use propagate().
-        2) Update the parameters using gradient descent rule for w and b.
-    """
-    
-    w = copy.deepcopy(w)
+
     b = copy.deepcopy(b)
     
     costs = []
     
     for i in range(num_iterations):
-        # (≈ 1 lines of code)
-        # Cost and gradient calculation 
-        # grads, cost = ...
-        # YOUR CODE STARTS HERE
         grads, cost = propagate(w,b,X,Y)
-        
-        # YOUR CODE ENDS HERE
-        
-        # Retrieve derivatives from grads
+
         dw = grads["dw"]
         db = grads["db"]
-        
-        # update rule (≈ 2 lines of code)
-        # w = ...
-        # b = ...
-        # YOUR CODE STARTS HERE
+
         w = w - learning_rate*dw
         b = b - learning_rate*db
-        
-        # YOUR CODE ENDS HERE
-        
-        # Record the costs
+
         if i % 100 == 0:
             costs.append(cost)
-        
-            # Print the cost every 100 training iterations
             if print_cost:
                 print ("Cost after iteration %i: %f" %(i, cost))
     
@@ -210,9 +171,6 @@ def optimize(w, b, X, Y, num_iterations=100, learning_rate=0.009, print_cost=Fal
     return params, grads, costs
 
 
-# In[62]:
-
-
 params, grads, costs = optimize(w, b, X, Y, num_iterations=100, learning_rate=0.009, print_cost=False)
 
 print ("w = " + str(params["w"]))
@@ -223,8 +181,6 @@ print("Costs = " + str(costs))
 
 optimize_test(optimize)
 
-
-# <a name='ex-7'></a>
 # ### Exercise 7 - predict
 # The previous function will output the learned w and b. We are able to use w and b to predict the labels for a dataset X. Implement the `predict()` function. There are two steps to computing predictions:
 # 
@@ -232,7 +188,6 @@ optimize_test(optimize)
 # 
 # 2. Convert the entries of a into 0 (if activation <= 0.5) or 1 (if activation > 0.5), stores the predictions in a vector `Y_prediction`. If you wish, you can use an `if`/`else` statement in a `for` loop (though there is also a way to vectorize this). 
 
-# In[78]:
 
 
 # GRADED FUNCTION: predict
